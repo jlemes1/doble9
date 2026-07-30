45b4a4a03916c006df12af90c9f11a8f9ff02b09
Juan Lemes <juanlemes77@gmail.com>
2026-07-29 22:35:32 -0300

    feat: fetch products

---
+++ src/actions/product.ts
@@ -0,0 +1,15 @@
+import { supabase } from '../supabase/client';
+
+export const getProducts = async () => {
+  const { data: products, error } = await supabase
+    .from('products')
+    .select('*')
+    .order('created_at', { ascending: false });
+
+  if (error) {
+    console.log(error.message);
+    throw new Error(error.message);
+  }
+
+  return products;
+};
